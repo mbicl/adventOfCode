6971feb89a9fc88e022b56f45439f7b3b6e745c8
mbicl <baxriddinovmaqsud@gmail.com>
2025-12-04 10:13:14 +0500

    2025/day4 C++ solution

--- 2025/day04/1.cpp
+++ 2025/day04/1.cpp
@@ -13,22 +13,32 @@ struct point {
     int x, y;
 };
 
-int dx[]={-1,0,0,1};
-int dy[]={0,-1,1,0};
+int dx[]={-1,0,0,1,-1,1,-1,1};
+int dy[]={0,-1,1,0,-1,-1,1,1};
 
 signed main() {
-	int a=50, ans=0;
+	vector<string> g;
 	string s;
+	g.push_back(string(500,'.'));
 	while (cin >> s) {
-		char c=s[0];
-		int x;
-		s.erase(0,1);
-		stringstream(s) >> x;
-		if (c=='L') a-=x;
-		else a+=x;
-		a+=100;
-		a%=100;
-		if (a==0) ans++;
+		s="."+s+".";
+		g.push_back(s);
+	}
+	g.push_back(string(500,'.'));
+	
+	int n = g.size();
+	int m = g[1].size();
+	int ans = 0;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (g[i][j]!='@') continue;
+			int cnt = 0;
+			for (int d = 0; d < 8; d++) {
+				if (g[i+dx[d]][j+dy[d]]=='@') cnt++;
+			}
+			if (cnt<4) ans++;
+		}
 	}
 
 	cout << ans;
--- 2025/day04/2.cpp
+++ 2025/day04/2.cpp
@@ -1,45 +1,53 @@
 #include "../../utils/aoc_utils.h"
 
 #include <iostream>
+#include <queue>
 #include <sstream>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
 int t = 100;
 
 struct point {
-    int x, y;
+	int x, y;
 };
 
-int dx[]={-1,0,0,1};
-int dy[]={0,-1,1,0};
+int dx[] = {-1, 0, 0, 1, -1, 1, -1, 1};
+int dy[] = {0, -1, 1, 0, -1, -1, 1, 1};
 
 signed main() {
-	int a=50, ans=0, cnt = 0;
+	vector<string> g;
 	string s;
+	g.push_back(string(500, '.'));
 	while (cin >> s) {
-		char c=s[0];
-		int x;
-		s.erase(0,1);
-		stringstream(s) >> x;
-		int old_a = a;
-		if (c=='L') {
-			while (x--) {
-				a--;
-				if (a==0) ans++;
-				if (a==-1) a=99;
-			}
-		}
-		else {
-			while (x--) {
-				a++;
-				if (a==100) a=0;
-				if (a==0) ans++;
-			}
-		}
+		s = "." + s + ".";
+		g.push_back(s);
 	}
+	g.push_back(string(500, '.'));
+	
+	int n = g.size();
+	int m = g[1].size();
+	int ans = 0;
+	int cnt = 0;
+
+	do {
+		cnt=0;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				if (g[i][j] != '@') continue;
+				int c = 0;
+				for (int d = 0; d < 8; d++) {
+					if (g[i + dx[d]][j + dy[d]] == '@') c++;
+				}
+				if (c < 4) {
+					g[i][j] = '.';
+					cnt++;
+				}
+			}
+		}
+		ans+=cnt;
+	} while (cnt > 0);
 
 	cout << ans;
 }
