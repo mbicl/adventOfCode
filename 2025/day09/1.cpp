fa05f6eb8f8feba171cb06dc7d5e87cf211a23be
mbicl <baxriddinovmaqsud@gmail.com>
2025-12-09 20:12:03 +0500

    2025/day9 C++ solution

--- 2025/day09/1.cpp
+++ 2025/day09/1.cpp
@@ -4,7 +4,7 @@
 #include <sstream>
 #include <vector>
 #include <queue>
-
+#define int long long
 using namespace std;
 
 int t = 100;
@@ -17,18 +17,25 @@ int dx[]={-1,0,0,1};
 int dy[]={0,-1,1,0};
 
 signed main() {
-	int a=50, ans=0;
 	string s;
-	while (cin >> s) {
-		char c=s[0];
-		int x;
-		s.erase(0,1);
-		stringstream(s) >> x;
-		if (c=='L') a-=x;
-		else a+=x;
-		a+=100;
-		a%=100;
-		if (a==0) ans++;
+	vector<point> vc;
+	while (getline(cin,s)) {
+		int x, y;
+		char c;
+
+		stringstream(s) >> x >> c >> y;
+
+		vc.push_back({x,y});
+	}
+
+	int ans = 0;
+
+	for (int i=0; i<vc.size(); ++i) {
+		for (int j=i+1; j<vc.size(); ++j) {
+			int l = abs(vc[i].x - vc[j].x+1);
+			int w = abs(vc[i].y - vc[j].y+1);
+			ans=max(ans, l*w);
+		}
 	}
 
 	cout << ans;
--- 2025/day09/2.cpp
+++ 2025/day09/2.cpp
@@ -4,42 +4,111 @@
 #include <sstream>
 #include <vector>
 #include <queue>
+#include <map>
+#include <set>
+
+#define int long long
+#define pii pair<int,int>
 
 using namespace std;
 
-int t = 100;
-
-struct point {
-    int x, y;
-};
-
-int dx[]={-1,0,0,1};
-int dy[]={0,-1,1,0};
-
 signed main() {
-	int a=50, ans=0, cnt = 0;
-	string s;
-	while (cin >> s) {
-		char c=s[0];
-		int x;
-		s.erase(0,1);
-		stringstream(s) >> x;
-		int old_a = a;
-		if (c=='L') {
-			while (x--) {
-				a--;
-				if (a==0) ans++;
-				if (a==-1) a=99;
-			}
-		}
-		else {
-			while (x--) {
-				a++;
-				if (a==100) a=0;
-				if (a==0) ans++;
-			}
-		}
-	}
+    string s;
+	vector<pii> vc;
+	while (getline(cin,s)) {
+		int x, y;
+		char c;
 
-	cout << ans;
+		stringstream(s) >> x >> c >> y;
+
+		vc.push_back({x,y});
+	}
+    
+    int n = vc.size();
+    set<int> sx, sy;
+    for (auto &[x,y]:vc) {
+        sx.insert(x);
+        sy.insert(y);
+    }
+    vector<int> xs(sx.begin(),sx.end());
+    vector<int> ys(sy.begin(),sy.end());
+    
+    map<int,int> mpx, mpy;
+    for (int i=0; i<xs.size(); ++i) mpx[xs[i]]=i;
+    for (int i=0; i<ys.size(); ++i) mpy[ys[i]]=i;
+    
+    int nx=xs.size(), ny=ys.size();
+
+    vector<array<int,4>> v;
+    for (int i=0; i<n; ++i) {
+        auto [x1,y1]=vc[i];
+        auto [x2,y2]=vc[(i+1)%n];
+        v.push_back({x1,y1,x2,y2});
+    }
+    
+    auto check=[&](int i, int j) -> bool {
+        if (i+1>=nx || j+1>=ny) return false;
+        double x=(xs[i]+xs[i+1])/2.0;
+        double y=(ys[j]+ys[j+1])/2.0;
+
+        int cnt=0;
+        for (auto &[x1,y1,x2,y2]:v) {
+            if (x1==x2) {
+                if (x1>x) {
+                    double y_min=min((double)y1,(double)y2);
+                    double y_max=max((double)y1,(double)y2);
+                    if (y_min<y && y<y_max) {
+                        cnt++;
+                    }
+                }
+            }
+        }
+        return cnt%2==1;
+    };
+    
+    vector<vector<bool>> inside(nx, vector<bool>(ny,false));
+    for (int i=0; i+1<nx; ++i) {
+        for (int j=0; j+1<ny; ++j) {
+            inside[i][j]=check(i,j);
+        }
+    }
+    
+    vector<vector<int>> pre(nx+1,vector<int>(ny+1,0));
+    for (int i=0; i<nx-1; ++i) {
+        for (int j=0; j<ny-1; ++j) {
+            pre[i+1][j+1]=pre[i][j+1]+pre[i+1][j]-pre[i][j]+(inside[i][j]?1:0);
+        }
+    }
+    
+    auto ok=[&](int i1, int j1, int i2, int j2) -> bool {
+        int s=(i2-i1)*(j2-j1);
+        int sum=pre[i2][j2]-pre[i1][j2]-pre[i2][j1]+pre[i1][j1];
+        return sum==s;
+    };
+    
+    int ans = 0;
+
+    for (int i=0; i<n; ++i) {
+        for (int j=i+1; j<n; ++j) {
+            auto [x1,y1]=vc[i];
+            auto [x2,y2]=vc[j];
+            if (x1==x2 || y1==y2) continue;
+            int xi1=mpx[x1], xi2=mpx[x2];
+            int yi1=mpy[y1], yi2=mpy[y2];
+            
+            if (xi1>xi2) swap(xi1,xi2);
+            if (yi1>yi2) swap(yi1,yi2);
+            
+            if (ok(xi1,yi1,xi2,yi2)) {
+                int xl=abs(x2-x1)+1;
+                int yl=abs(y2-y1)+1;
+                int s=xl*yl;
+                ans=max(ans,s);
+            }
+        }
+    }
+    
+    cout << ans << endl;
+    
+    return 0;
 }
